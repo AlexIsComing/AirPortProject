305d49c69f7b782e355eb98bdf7248b281e3c4d3
AlexIsComing <alexia.blum@efrei.net>
2022-02-12 04:47:00 -0800

    airport + runways

--- src/main/scala/fileReader/model/airport.scala
+++ src/main/scala/fileReader/model/airport.scala
@@ -2,4 +2,62 @@ package fileReader.model
 
 import scala.util.Try
 
-final case class airport()
+case class Airport(id: Int,
+                   ident: String,
+                   Type: String,
+                   name: String,
+                   latitude_deg: double,
+                   longitude_deg: double,
+                   elevation_ft: double,
+                   continent: String,
+                   iso_country: String,
+                   iso_region: String,
+                   municipality: String,
+                   scheduled_service: option[String] = None,
+                   gps_code: option[String] = None,
+                   iata_code: option[String] = None,
+                   local_code: option[String] = None,
+                   home_link: option[String] = None,
+                   wikipedia_link: option[String] = None,
+                   keywords: option[String] = None)
+
+object Airport {
+  def fromCsvLine(line: Array[String]): Option[Airport] = {
+    parseAirport(line)
+  }
+
+  def parseAirport(line: Array[String]): Option[Airport] = {
+    (Try(line(0).toInt).toOption,
+      Try(line(1)).toOption,
+      Try(line(2)).toOption,
+      Try(line(3)).toOption,
+      Try(line(4)).toOption,
+      Try(line(5)).toOption)
+      Try(line(6)).toOption)
+      Try(line(7)).toOption)
+      Try(line(8)).toOption)
+      Try(line(9)).toOption)
+      Try(line(10)).toOption)
+      Try(line(11)).toOption)
+      Try(line(12)).toOption)
+      Try(line(13)).toOption)
+      Try(line(14)).toOption)
+      Try(line(15)).toOption)
+      Try(line(16)).toOption)
+      Try(line(17)).toOption) match {
+
+
+      case (Some(id), Some(Type), Some(name), Some(latitude_deg), Some(longitude_deg), Some(elevation_ft),
+      Some(continent), Some(iso_country), Some(iso_region), Some(municipality), Some(scheduled_service), Some(gsp_code),
+      Some(iata_code), Some(local_link), Some(wikipedia_link), Some(keywords)
+      => Some(Airport(id, ident, Type, name,
+         latitude_deg, longitude_deg, elevation_ft, continent, iso_country, iso_region, municipality,
+         scheduled_service, gps_code, local_code, home_link, wikipedia_link, keywords))
+
+      case _ => None
+    }
+  }
+}
+
+
+
--- src/main/scala/fileReader/model/runway.scala
+++ src/main/scala/fileReader/model/runway.scala
@@ -1,3 +1,66 @@
 package fileReader.model
 
-case class runway()
+import scala.util.Try
+
+final case class Runway(id: Int,
+                        airport_ref: Int,
+                        airport_ident: String,
+                        length_ft: Int,
+                        width_ft: Int,
+                        surface: String,
+                        lighted: Int,
+                        closed: Int,
+                        le_ident: String,
+                        le_latitude_deg: Option[Float],
+                        le_longitude_deg: Option[Float],
+                        le_elevation_ft: Option[Int],
+                        le_heading_degT: Option[Float],
+                        le_displaced_threshold_ft: Option[Int],
+                        he_ident: Option[String],
+                        he_latitude_deg: Option[Float],
+                        he_longitude_deg: Option[Float],
+                        he_elevation_ft: Option[Int],
+                        he_heading_degT: Option[Float],
+                        he_displaced_threshold_ft: Option[Int])
+
+object Runway{
+
+  def fromCsvLine(line: Array[String]): Option[Runway] = {
+  parseRunway(line)
+}
+
+  def parseRunway: Array[String]): Option[Runway] = {
+  (Try(line(0).toInt).toOption,
+  Try(line(1)).toInt,
+  Try(line(2)).toOption,
+  Try(line(3)).toInt,
+  Try(line(4)).toInt,
+  Try(line(5)).toOption)
+  Try(line(6)).toOption)
+  Try(line(7)).toOption)
+  Try(line(8)).toOption)
+  Try(line(9)).toOption)
+  Try(line(10)).toOption)
+  Try(line(11)).toOption)
+  Try(line(12)).toOption)
+  Try(line(13)).toOption)
+  Try(line(14)).toOption)
+  Try(line(15)).toOption)
+  Try(line(16)).toOption)
+  Try(line(17)).toOption)
+  Try(line(18)).toOption)
+  Try(line(19)).toOption)
+  Try(line(20)).toOptionmatch) match {
+
+      case (Some(id), Some(code), Some(name), Some(continent), Some(wikipedia_link), Some(keywords))
+      => Some(Country(id, code, name, continent, wikipedia_link, Some(keywords)))
+      case (Some(id), Some(code), Some(name), Some(continent), Some(wikipedia_link), None)
+      => Some(Country(id, code, name, continent, wikipedia_link))
+      case _ => None
+
+    }
+  }
+
+}
+
+
